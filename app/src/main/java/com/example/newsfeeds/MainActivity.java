186950acfac7dbb6166cff55adc5b7009ccee4b3
Mihir Shirgaonkar <shirgaonkarmihir14@gmail.com>
2021-02-08 00:09:49 +0530

    new launcher icon is added and new countries are added in search view

--- app/src/main/java/com/example/newsfeeds/MainActivity.java
+++ app/src/main/java/com/example/newsfeeds/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.newsfeeds;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.newsfeeds.NewsClasses.Articles;
@@ -19,6 +28,9 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spinner;
+    String countryName = "in";
 
     RecyclerView recyclerView;
     final String API_KEY = "917f03499c644394a5dff8579a9c8bdb";
@@ -35,12 +47,111 @@ public class MainActivity extends AppCompatActivity {
 
         String country = getCountry();
         retrieveJson(country,API_KEY);
+
+        spinner = findViewById(R.id.spinner);
+
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Select Region");
+        list.add("India");
+        list.add("United States");
+        list.add("United Kingdom");
+        list.add("Turkey");
+        list.add("Switzerland");
+        list.add("South Africa");
+        list.add("Singapore");
+        list.add("Saudi Arabia");
+        list.add("Australia");
+
+
+        spinner.setAdapter(new ArrayAdapter<>(MainActivity.this,
+                android.R.layout.simple_spinner_dropdown_item,list));
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                switch (position){
+                    case 0:
+
+                        break;
+                    case 1:
+                        countryName = "in";
+                        String country = getCountry();
+                        retrieveJson(country,API_KEY);
+
+                        break;
+
+                        case 2:
+                        countryName = "us";
+                            country = getCountry();
+                            retrieveJson(country,API_KEY);
+
+                            break;
+
+                            case 3:
+                        countryName = "gb";
+                            country = getCountry();
+                            retrieveJson(country,API_KEY);
+
+                            break;
+
+                            case 4:
+                        countryName = "tr";
+                            country = getCountry();
+                            retrieveJson(country,API_KEY);
+
+                            break;
+
+                        case 5:
+                        countryName = "ch";
+                            country = getCountry();
+                            retrieveJson(country,API_KEY);
+
+                            break;
+
+                        case 6:
+                        countryName = "za";
+                            country = getCountry();
+                            retrieveJson(country,API_KEY);
+
+                            break;
+
+                            case 7:
+                        countryName = "sg";
+                            country = getCountry();
+                            retrieveJson(country,API_KEY);
+
+                            break;
+ case 8:
+                        countryName = "sa";
+                            country = getCountry();
+                            retrieveJson(country,API_KEY);
+
+                            break;
+case 9:
+                        countryName = "au";
+                            country = getCountry();
+                            retrieveJson(country,API_KEY);
+
+                            break;
+
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 
     public String getCountry(){
-        Locale locale = Locale.getDefault();
-        String country = locale.getCountry();
-        return country.toLowerCase();
+//        Locale locale = Locale.getDefault();
+//        String country = locale.getCountry();
+        return countryName;
     }
 
     public void retrieveJson(String country,String apiKey){
